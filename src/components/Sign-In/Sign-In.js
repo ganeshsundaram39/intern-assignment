f7508ef9dd9857b05d590e6e87d0756376cb31e2
ganeshsundaram39 <ganeshsundaram39@gmail.com>
2019-03-14 23:07:02 +0530

    sign-in-page

---
+++ src/components/Sign-In/Sign-In.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+// import PropTypes from 'prop-types'
+import './Sign-In.css';
+import LinkedInButton from '../../resources/images/signin-button.png';
+export default class SignIn extends Component {
+  // static propTypes = {
+
+  // }
+  liLogin = () => {
+    // Setup an event listener to make an API call once auth is complete
+    window.IN.User.authorize(response => {
+      console.log(response);
+    });
+    window.IN.Event.on(window.IN, 'auth', this.getProfileData);
+  };
+  getProfileData = () => {
+    // Use the API call wrapper to request the member's basic profile data
+    window.IN.API.Profile('me')
+      .fields(
+        'id,firstName,emailAddress,lastName,picture-urls::(original),public-profile-url,location:(name)'
+      )
+      .result(function(me) {
+        var profile = me.values[0];
+        console.log(profile);
+      });
+  };
+  render() {
+    return (
+      <div className="SignIn">
+        <button onClick={this.liLogin}>
+          <img src={LinkedInButton} alt="signIn" />
+        </button>
+      </div>
+    );
+  }
+}
