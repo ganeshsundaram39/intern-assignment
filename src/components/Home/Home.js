9cfe44266dc9b313058b6d9f1fe9bec2e2037a7d
ganeshsundaram39 <ganeshsundaram39@gmail.com>
2019-03-16 22:40:25 +0530

    worked on home section

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 // import PropTypes from 'prop-types'
-
+import './Home.css';
 export default class Home extends Component {
   //   static propTypes = {
   //     prop: PropTypes
@@ -8,6 +8,27 @@ export default class Home extends Component {
 
   render() {
     console.log(this.props.userInfo);
-    return <div>Ganesh will rock.</div>;
+    return (
+      <div className="Home">
+        <div className="profile-pic-wrapper">
+          <img src={this.props.userInfo.pictureUrls.values[0]} alt="" />
+        </div>
+        <div className="other-info">
+          <div className="name">
+            {this.props.userInfo.firstName + ' ' + this.props.userInfo.lastName}
+          </div>
+          <div className="location">{this.props.userInfo.location.name}</div>
+          <div className="public-profile">
+            <a
+              href={this.props.userInfo.publicProfileUrl}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Public Profile
+            </a>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
